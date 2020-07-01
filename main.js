6bc00a3bcc1c63589477d542dc5cb21c99c13f4a
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-07-01 10:43:44 -0400

    almost

--- main.js
+++ main.js
@@ -1,6 +1,131 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const indexedChars = function(str){
+  let result = '';
+  for(let i = 0; i < str.length; i++){
+    result += i + str[i];
+  }
+  return result;
+}
+
+const exclaim = function(str){
+  let yelling = ''
+  for (let i = 0; i < str.length; i++){
+    if  (str[i] === '?' || str[i] === '.'){
+      yelling += '!'
+    }else{
+      yelling  += str[i]
+    }
+  }
+return yelling;
+}
+
+const repeatIt = function(str, num){
+  let repeatStr = ''
+  while(num > 0){
+    repeatStr += str;
+    num--;
+  }
+  return repeatStr;
+}
+
+const truncate = function(str){
+  let result = ''
+  for (let i = 0; i < str.length; i++){
+    if (str[i] >= 15){
+      result += '...'
+    } else{
+      result += str[i]
+    }
+  }
+  return result;
+}
+
+const ciEmailify = function(name){
+  let email = ' '
+  for (let i = 0; i < name.length; i++){
+    if(name[i] === ' '){
+      email += '.'
+    }else{
+      email += name[i].toLowerCase;
+    }
+  }
+  return email
+}
+
+const reverse = function(str){
+let result = ''
+  for(let i = str.length - 1; i >= 0; i--){
+    result += str[i]
+  } 
+  return result;
+}
+
+const onlyVowels = function(str){
+  let result = ''
+  for(let i = 0; i < str.length; i ++){
+    if ('aeiouAEIOU'.includes(str[i])){
+      result += str[i]
+    }
+  }
+    return result;
+}
+
+const numberedChars = function(str){
+  let result = ''
+  for(let i = 0; i < str.length; i ++){
+
+  }
+}
+
+const crazyCase = function(str){
+  let result = ''
+  for(let i = 0; i < str.length; i ++){
+    if (i % 2 !== 0){
+      result += str[i].toUpperCase();
+    }else{
+      result += str[i].toLowerCase();
+    }
+  }
+  return result;
+}
+
+const titleCase = function(str){
+  let result = ''
+  for(let i = 0; i < str.length; i ++){
+    if (str[i -1]=== ' ' || i ===0){
+      result += str[i].toUpperCase();
+    }else{
+      result += str[i].toLowerCase();
+    }
+  }
+  return result;
+}
+
+const camelCase = function(str){
+  let result = ''
+  for(let i = 0; i < str.length; i ++){
+
+  }
+
+}
+
+const crazyCase2ReturnOfCrazyCase = function(str){
+  let result = ''
+  let courter = 1
+  for(let i = 0; i < str.length; i ++){
+    if (str[i] !== ' '){
+      courter++;
+    }
+    if(courter % 2 !==0){
+      result += str[i].toUpperCase();
+    }else{
+      result += str[i].toLowerCase(); 
+    }
+  }
+  return result;
+}
 
 
 
@@ -10,7 +135,7 @@
 
 
 
- /********************************************
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
